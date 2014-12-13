9c195ad78d5aeabc3f34fd36b1bb215df52c2062
Judith Barrett <judith.barrett@gmail.com>
2014-12-12 20:12:17 -0700

    Added Judith page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   def index
   end
   def bye
+
+  def judith
+
   end
 
   def jason
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/judith' => 'welcome#judith'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
